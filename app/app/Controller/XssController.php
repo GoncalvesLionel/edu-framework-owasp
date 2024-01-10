5dd2450bee42253902d32ad0ba1398b96986b45e
JPechberty <j.pechberty@gmail.com>
2024-01-10 01:24:37 +0100

    init repo and add xss 101 example

---
+++ app/app/Controller/XssController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Controller;
+
+use Studoo\EduFramework\Core\Controller\ControllerInterface;
+use Studoo\EduFramework\Core\Controller\Request;
+use Studoo\EduFramework\Core\View\TwigCore;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class XssController implements ControllerInterface
+{
+    /**
+     * @param Request $request Requête HTTP
+     * @return string|null
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function execute(Request $request): string|null
+    {
+
+        if($request->getHttpMethod() === "GET" && isset($_GET["text"])){
+            $text = $_GET["text"];
+        }
+
+        return TwigCore::getEnvironment()->render(
+            'xss/xss.html.twig',
+            [
+                'titre'   => 'OWASP XSS',
+                'requete' => $request,
+                'text' =>  $_GET["text"]
+            ]
+        );
+    }
+}
