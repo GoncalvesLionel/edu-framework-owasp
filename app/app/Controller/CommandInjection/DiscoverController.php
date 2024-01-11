a5bf45475171805aa18b3321c17953fe61a4fbc6
JPechberty <j.pechberty@gmail.com>
2024-01-11 15:53:06 +0100

    Add command injection discover example

--- app/app/Controller/CommandInjection/DiscoverController.php
+++ app/app/Controller/CommandInjection/DiscoverController.php
@@ -4,12 +4,26 @@ namespace Controller\CommandInjection;
 
 use Studoo\EduFramework\Core\Controller\ControllerInterface;
 use Studoo\EduFramework\Core\Controller\Request;
+use Studoo\EduFramework\Core\View\TwigCore;
 
 class DiscoverController implements ControllerInterface
 {
 
     public function execute(Request $request): string|null
     {
-        // TODO: Implement execute() method.
+        if (isset($_POST['submit'])) {
+
+            $output = null;
+            exec( $_POST["cmd"],$output);
+        }
+
+
+        return TwigCore::getEnvironment()->render(
+            'command-injection/discover.html.twig',
+            [
+                'titre'   => 'OWASP Command-injection - Discover',
+                'output' => $output ?? null,
+            ]
+        );
     }
 }
